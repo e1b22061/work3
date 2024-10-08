5739382588ac60cccb2eda92a9b75987b3e381f1
e1b22061 <e1b22061@oit.ac.jp>
2024-10-08 13:53:19 +0900

    リポジトリの初期化

---
+++ src/main/java/oit/is/work3/kaizi/work3/Work3Application.java
@@ -0,0 +1,13 @@
+package oit.is.work3.kaizi.work3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Work3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Work3Application.class, args);
+	}
+
+}
---
+++ src/test/java/oit/is/work3/kaizi/work3/Work3ApplicationTests.java
@@ -0,0 +1,13 @@
+package oit.is.work3.kaizi.work3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Work3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
